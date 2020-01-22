7f1d0dea48e79a153ce45cc1adb9be87707b9a94
hongdazhou <zhouhongda@gmail.com>
2020-01-21 21:04:25 -0500

    Seq Troubleshooting

---
+++ models/cart.js
@@ -0,0 +1,109 @@
+const Sequelize = require("sequelize");
+
+const sequelize = require("../util/database");
+
+const Cart = sequelize.define("cart", {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true
+  }
+});
+
+module.exports = Cart;
+
+// const fs = require("fs");
+// const path = require("path");
+
+// const p = path.join(
+//   path.dirname(process.mainModule.filename),
+//   "data",
+//   "cart.json"
+// );
+// const getCartFromFile = cb => {
+//   fs.readFile(p, (err, fileContent) => {
+//     if (err) {
+//       cb([]);
+//     } else {
+//       cb(JSON.parse(fileContent));
+//     }
+//   });
+// };
+
+// module.exports = class Cart {
+//   static addProduct(id, productPrice) {
+//     //Fetch the previous Cart
+//     fs.readFile(p, (err, fileContent) => {
+//       let cart = { products: [], totalPrice: 0 };
+//       if (!err) {
+//         cart = JSON.parse(fileContent);
+//       }
+//       //Analyze the cart => Find existing Product
+//       const existingProdcutIndex = cart.products.findIndex(
+//         prod => prod.id === id
+//       );
+//       const existingProduct = cart.products[existingProdcutIndex];
+
+//       let updatedProduct;
+//       //Add new product/ Increase Quantity.
+//       if (existingProduct) {
+//         updatedProduct = { ...existingProduct };
+//         updatedProduct.qty = updatedProduct.qty + 1;
+//         cart.products = [...cart.products];
+//         cart.products[existingProdcutIndex] = updatedProduct;
+//       } else {
+//         updatedProduct = { id: id, qty: 1 };
+//         cart.products = [...cart.products, updatedProduct];
+//       }
+//       cart.totalPrice = cart.totalPrice + +productPrice;
+//       fs.writeFile(p, JSON.stringify(cart), err => {
+//         console.log(err);
+//       });
+//     });
+//   }
+
+//   static fetchAll(cb) {
+//     getCartFromFile(cb);
+//   }
+
+//   static findById(id, cb) {
+//     getProductsFromFile(products => {
+//       const product = products.find(p => p.id === id);
+//       cb(product);
+//     });
+//   }
+
+//   static deleteProduct(id, productPrice) {
+//     fs.readFile(p, (err, fileContent) => {
+//       if (err) {
+//         return;
+//       }
+//       const updatedCart = { ...JSON.parse(fileContent) };
+//       const product = updatedCart.products.find(prod => prod.id === id);
+//       if (!product) {
+//         return;
+//       }
+//       const productQty = product.qty;
+//       updatedCart.totalPrice =
+//         updatedCart.totalPrice - productPrice * productQty;
+//       updatedCart.products = updatedCart.products.filter(
+//         prod => prod.id !== id
+//       );
+//       fs.writeFile(p, JSON.stringify(updatedCart), err => {
+//         console.log(err);
+//       });
+//     });
+//   }
+
+//   static getCart(cb) {
+//     fs.readFile(p, (err, fileContent) => {
+//       const cart = JSON.parse(fileContent);
+//       if (err) {
+//         cb(null);
+//       } else {
+//         cb(cart);
+//       }
+//     });
+//   }
+// };
